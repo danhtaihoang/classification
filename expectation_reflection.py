a154cc6a569bf69c3a1c7ecddb00f8fb095ad300
danhtaihoang <hoangdanhtai@gmail.com>
2020-01-16 15:34:14 -0500

    f1 score

--- expectation_reflection.py
+++ expectation_reflection.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy import linalg
 from sklearn.preprocessing import OneHotEncoder
 from scipy.special import erf as sperf
+from sklearn.linear_model import ElasticNet
 
 #=============================================================================================================
 def infer_LAD(x, y, regu=0.1,tol=1e-8, max_iter=5000):
@@ -60,6 +61,67 @@ def infer_LAD(x, y, regu=0.1,tol=1e-8, max_iter=5000):
     #return b_sol,w_sol 
     return b_sol[0][0],w_sol[:,0] # for only one target case
 
+#=============================================================================================================
+# 2020.01.15: find w and h0 by ElasticNet
+def fit_ElasticNet(x,y,niter_max,alpha,l1_ratio):      
+    # convert 0, 1 to -1, 1
+    y1 = 2*y - 1.
+   
+    #print(niter_max)    
+    n = x.shape[1]
+    
+    #x_av = np.mean(x,axis=0)
+    #dx = x - x_av
+    #c = np.cov(dx,rowvar=False,bias=True)
+
+    # 2019.07.16:  
+    #c += l2*np.identity(n) / (2*len(y))
+    #c_inv = linalg.pinvh(c)
+
+    # initial values
+    h0 = 0.
+    w = np.random.normal(0.0,1./np.sqrt(n),size=(n))
+    
+    cost = np.full(niter_max,100.)
+
+    model = ElasticNet(random_state=0,alpha=alpha,l1_ratio=l1_ratio)
+
+    for iloop in range(niter_max):
+        h = h0 + x.dot(w)
+        y1_model = np.tanh(h/2.)    
+
+        # stopping criterion
+        p = 1/(1+np.exp(-h))                
+        cost[iloop] = ((p-y)**2).mean()
+
+        #h_test = h0 + x_test.dot(w)
+        #p_test = 1/(1+np.exp(-h_test))
+        #cost[iloop] = ((p_test-y_test)**2).mean()
+
+        if iloop>0 and cost[iloop] >= cost[iloop-1]: break
+
+        # update local field
+        t = h!=0    
+        h[t] *= y1[t]/y1_model[t]
+        h[~t] = 2*y1[~t]
+
+        # find w from h    
+        #h_av = h.mean()
+        #dh = h - h_av 
+        #dhdx = dh[:,np.newaxis]*dx[:,:]
+        #dhdx_av = dhdx.mean(axis=0)
+        #w = c_inv.dot(dhdx_av)
+        #h0 = h_av - x_av.dot(w)
+
+        # 2019.12.26: 
+        #h0,w = infer_LAD(x,h[:,np.newaxis],l2)
+
+        # 2020.01.15: find w and h0 by ElasticNet
+        model.fit(x,h)
+        w = model.coef_
+        h0 = model.intercept_
+        
+    return h0,w
 #=============================================================================================================
 def fit_LAD(x,y,niter_max,l2):      
     # convert 0, 1 to -1, 1
